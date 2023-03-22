8a29698a57045333a71c45adc5801e9fa6c8f9f5
IsacAndersson2 <isac.andersson2@elev.ga.lbs.se>
2023-03-22 20:27:13 +0100

    SpawnEnemies script fixed

--- Assets/Scripts/Enemies/Spawnenemies.cs
+++ Assets/Scripts/Enemies/Spawnenemies.cs
@@ -5,25 +5,39 @@ using UnityEngine;
 public class Spawnenemies : MonoBehaviour
 {
 
-    [SerializeField] private GameObject _enemyPrefab;
+    public GameObject enemyPrefab;
+    public GameObject player;
 
     [SerializeField] private float _spawnIntervall;
+    public float _spawnRange;
     private float _timeSinceLastSpawn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnAnEnemy()); 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        _timeSinceLastSpawn += Time.deltaTime;
+    //void Update()
+    //{
+    //    _timeSinceLastSpawn += Time.deltaTime;
 
-        if(_timeSinceLastSpawn >= _spawnIntervall)
-        {
-            GameObject newEnemy = Instantiate(_enemyPrefab, new Vector3(Random.Range(5f, 5f), Random.Range(-6f, 6f), 0), Quaternion.identity);
-            _timeSinceLastSpawn = 0;
-        }
+    //    if (_timeSinceLastSpawn >= _spawnIntervall)
+    //    {
+    //        GameObject newEnemy = Instantiate(enemyPrefab, new Vector3(Random.Range(5f, 5f), Random.Range(-6f, 6f), 0), Quaternion.identity);
+    //        _timeSinceLastSpawn = 0;
+            
+            
+    //    }
+    //}
+ 
+    IEnumerator SpawnAnEnemy()
+    {
+        Vector2 spawnPos = player.transform.position;
+        spawnPos += Random.insideUnitCircle.normalized * _spawnRange;
+
+        Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+        yield return new WaitForSeconds(_spawnIntervall);
+        StartCoroutine(SpawnAnEnemy());
     }
 }
