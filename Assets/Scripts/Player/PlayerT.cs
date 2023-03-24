cd6deb2ea5c9f545ff865da4becc9867202e4627
Sleeperdog <vfrannhag@gmail.com>
2023-03-24 09:19:54 +0100

    Random shit

---
+++ Assets/Scripts/Player/PlayerT.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerT : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+
+    private Rigidbody2D rb;
+
+    private Vector2 movement;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+}
