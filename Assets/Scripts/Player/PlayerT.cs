accc434bd91ed6b508040401587d386c6e28b3a9
Sleeperdog <vfrannhag@gmail.com>
2023-03-24 11:27:12 +0100

    Movement fixed

--- Assets/Scripts/Player/PlayerT.cs
+++ Assets/Scripts/Player/PlayerT.cs
@@ -2,35 +2,48 @@ using UnityEngine;
 
 public class PlayerT : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float moveSpeed = 100f;
     [SerializeField] private float rotationSpeed = 700f;
 
     private Rigidbody2D rb;
-    private Vector2 movement;
+    private Vector2 forwardDirection;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        forwardDirection = transform.up;
     }
 
     private void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.MovePosition(rb.position + (moveSpeed * 4) * Time.deltaTime * forwardDirection);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            forwardDirection = Quaternion.Euler(0, 0, rotationSpeed * Time.deltaTime) * forwardDirection;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            forwardDirection = Quaternion.Euler(0, 0, -rotationSpeed * Time.deltaTime) * forwardDirection;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.MovePosition(rb.position - forwardDirection * (moveSpeed*2) * Time.deltaTime);
+        }
     }
 
     private void FixedUpdate()
     {
-        if (movement.magnitude > 0)
-        {
-            rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+        // Calculate the angle between the current facing direction and the movement direction
+        float angle = Mathf.Atan2(forwardDirection.y, forwardDirection.x) * Mathf.Rad2Deg - 90f;
+        Quaternion targetRotation = Quaternion.Euler(0f, 0f, angle);
 
-            // Calculate the angle between the current facing direction and the movement direction
-            float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg - 90f;
-            Quaternion targetRotation = Quaternion.Euler(0f, 0f, angle);
-
-            // Rotate the player towards the movement direction
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
-        }
+        // Rotate the player towards the movement direction
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
     }
 }
