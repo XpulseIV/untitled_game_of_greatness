cc899786002983a8128d76b6fd573205fd3cf29a
Sleeperdog <vfrannhag@gmail.com>
2023-04-14 10:56:20 +0200

    musen jaaa :trollface:

---
+++ Assets/Scripts/Player/Gem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    void Start()
+    {
+         Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+
+        transform.position = mousePos;
+    }
+}
