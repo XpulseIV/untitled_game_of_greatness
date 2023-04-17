aed255527c456bae59ebd272b6d378a3778f96f1
IsacAndersson2 <isac.andersson2@elev.ga.lbs.se>
2023-04-17 15:44:25 +0200

    Sync M-player

--- Assets/Scripts/Buildibles/ActivateSelf.cs
+++ Assets/Scripts/Buildibles/ActivateSelf.cs
@@ -7,14 +7,17 @@ public class ActivateSelf : MonoBehaviour
     private GameObject _activatedObject;
     [SerializeField] private string _objectTag;
     [SerializeField] private float _activasionRange;
+    [SerializeField] private string _normalTag;
     public bool isActivated = false;
     private Vector2 _activasionDistance;
+    public GameObject mspelaren;
+    private CustomCursor _customCursor;
 
 
 
     void Start()
     {
-        
+        _customCursor = mspelaren.GetComponent<CustomCursor>();
     }
 
     
@@ -32,6 +35,17 @@ public class ActivateSelf : MonoBehaviour
         else
         {
             isActivated = false;
-        }        
+        }
+
+        if (_customCursor.inUtility == true && Input.GetKey("e"))
+        {
+            gameObject.tag = _normalTag;
+            Invoke("DeactivateInUtilty", 0.1f);
+        }
+    }
+
+    private void DeactivateInUtilty()
+    {
+        _customCursor.inUtility = false;
     }
 }
