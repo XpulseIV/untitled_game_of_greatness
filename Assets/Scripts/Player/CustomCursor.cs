7a829000b6518e18e48330b4c738fe6a1ef15261
Sleeperdog <vfrannhag@gmail.com>
2023-04-17 15:07:31 +0200

    Huge updates sheesh

--- Assets/Scripts/Player/CustomCursor.cs
+++ Assets/Scripts/Player/CustomCursor.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class CustomCursor : MonoBehaviour
 {
+    [SerializeField] public Sprite normalCursor;
+    [SerializeField] public Sprite utilityCursor;
+
+    [SerializeField] public bool inUtility;
+
     void Start()
     {
         Cursor.visible = false;
@@ -16,4 +21,26 @@ public class CustomCursor : MonoBehaviour
 
         transform.position = mousePos;
     }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKey(KeyCode.E)) // check if left mouse button is pressed down
+        {
+            if (collision.gameObject.tag == "Traps")
+            {
+                Debug.Log("Collided with trap");
+                collision.gameObject.tag = "ActivatedTrap";
+
+            }
+            else if (collision.gameObject.tag == "ConveyourBelt")
+            {
+                // do something for conveyour belts
+            }
+            else if (collision.gameObject.tag == "Turrets")
+            {
+                // do something for turrets
+            }
+        }
+    }
+
 }
