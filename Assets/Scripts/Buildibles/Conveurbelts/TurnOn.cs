5ab9dda48047b4e73dd105a2eed235d1072719cf
IsacAndersson2 <isac.andersson2@elev.ga.lbs.se>
2023-04-19 17:36:54 +0200

    Some bug fixes and animation, but not enough

--- Assets/Scripts/Buildibles/Conveurbelts/TurnOn.cs
+++ Assets/Scripts/Buildibles/Conveurbelts/TurnOn.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TurnOn : MonoBehaviour
+
 {
     private AreaEffector2D _areaEffector2D;
     private ActivateSelf _activateSelf;
+    private Animator _animator;
     
     // Start is called before the first frame update
     void Start()
     {
         _areaEffector2D = GetComponent<AreaEffector2D>();
         _activateSelf = GetComponent<ActivateSelf>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,10 +23,12 @@ public class TurnOn : MonoBehaviour
         if (_activateSelf.isActivated)
         {
             _areaEffector2D.enabled = true;
+            _animator.SetBool("IsOn", true);
         }
         else
         {
             _areaEffector2D.enabled = false;
+            _animator.SetBool("IsOn", false);
         }
     }
 }
--- Assets/Scripts/Buildibles/Traps/TrapStats.cs
+++ Assets/Scripts/Buildibles/Traps/TrapStats.cs
@@ -9,15 +9,32 @@ public class TrapStats : MonoBehaviour
     public float burstSpeed;
     public float burstDamage;
 
+    private Animator _animator;
+    private ActivateSelf _activateSelf;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
+        _activateSelf = GetComponent<ActivateSelf>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(_activateSelf.isActivated)
+        {
+            _animator.SetBool("IsOn", true);
+
+            if (Input.GetKey("space"))
+            {
+                _animator.SetBool("IsInBurst", true);
+                _animator.SetBool("IsInBurst", false);
+            }
+        }
+        else
+        {
+            _animator.SetBool("IsOn", false);
+        }
     }
 }
