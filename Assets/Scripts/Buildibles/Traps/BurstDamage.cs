6fbee232ee3df5bbfdde8ea9242a81e5ae0054f8
IsacAndersson2 <isac.andersson2@elev.ga.lbs.se>
2023-04-21 11:32:59 +0200

    Sync

--- Assets/Scripts/Buildibles/Traps/BurstDamage.cs
+++ Assets/Scripts/Buildibles/Traps/BurstDamage.cs
@@ -1,52 +1,125 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class BurstDamage : MonoBehaviour
+//{
+//    public EnemyStats enemyStats;
+//    //public float enemyHealth;
+//    private bool _hasTakenDamage;
+//    public GameObject traps;
+//    private TrapStats _trapStats;
+//    private ActivateSelf _activateSelf;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        _trapStats = traps.GetComponent<TrapStats>();
+//        enemyStats = GetComponent<EnemyStats>();
+//        _activateSelf = GetComponent<ActivateSelf>();
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+//    }
+
+//    private void OnTriggerStay2D(Collider2D collision)
+//    {
+//        if (_activateSelf.isActivated && Input.GetKey("space"))
+//        {
+//            if (collision.gameObject.tag == "Enemy")
+//            {
+//                if (!_hasTakenDamage)
+//                {
+//                    enemyStats.enemyHealth -= _trapStats.burstDamage;
+//                    _hasTakenDamage = true;
+//                    Invoke("HasTakenDamage", _trapStats.burstSpeed);
+//                }
+
+//                if (enemyStats.enemyHealth <= 0)
+//                {
+//                    Destroy(gameObject, 0);
+//                }
+//            }
+//        }
+//    }
+//    private void HasTakenDamage()
+//    {
+//        _hasTakenDamage = false;
+//    }
+//}
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
+using UnityEngine.UIElements;
 
 public class BurstDamage : MonoBehaviour
 {
-    public EnemyStats enemyStats;
-    //public float enemyHealth;
-    private bool _hasTakenDamage;
-    public GameObject traps;
     private TrapStats _trapStats;
     private ActivateSelf _activateSelf;
 
-    // Start is called before the first frame update
+    //public float burstDamage = 10f;
+    //public float burstSpeed = 0.5f;
+
+    private bool _hasActivated;
+
     void Start()
     {
-        _trapStats = traps.GetComponent<TrapStats>();
-        enemyStats = GetComponent<EnemyStats>();
+        _trapStats = GetComponent<TrapStats>();
         _activateSelf = GetComponent<ActivateSelf>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
+        Debug.Log("OnTriggerEnter2D");
+
         if (_activateSelf.isActivated && Input.GetKey("space"))
         {
-            if (collision.gameObject.tag == "Traps")
+            Debug.Log("Trap activated");
+
+            if (collision.gameObject.CompareTag("Enemy") && !_hasActivated)
             {
-                if (!_hasTakenDamage)
+                Debug.Log("Enemy hit by trap");
+
+                _hasActivated = true;
+
+                var enemyStats = collision.gameObject.GetComponent<EnemyStats>();
+                if (enemyStats != null)
                 {
                     enemyStats.enemyHealth -= _trapStats.burstDamage;
-                    _hasTakenDamage = true;
-                    Invoke("HasTakenDamage", _trapStats.burstSpeed);
-                }
 
-                if (enemyStats.enemyHealth <= 0)
-                {
-                    Destroy(gameObject, 0);
+                    if (enemyStats.enemyHealth <= 0)
+                    {
+                        Destroy(collision.gameObject);
+                    }
+                    else
+                    {
+                        Invoke("ResetActivation", _trapStats.burstSpeed);
+                    }
                 }
             }
         }
+        
+        
     }
-    private void HasTakenDamage()
+
+    private void ResetActivation()
     {
-        _hasTakenDamage = false;
+        Debug.Log("ResetActivation");
+
+        _hasActivated = false;
     }
 }
+
+
+
+
+
+
+
