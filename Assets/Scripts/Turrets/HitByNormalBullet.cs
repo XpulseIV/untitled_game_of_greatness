d55a5076c8a687f8d98ffbd6023420b5b164deee
IsacAndersson2 <isac.andersson2@elev.ga.lbs.se>
2023-05-12 11:31:30 +0200

    Sync

---
+++ Assets/Scripts/Turrets/HitByNormalBullet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class HitByNormalBullet : MonoBehaviour
+{
+    private EnemyStats _enemyStats;
+    public float normalBulletDamage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemyStats = GetComponent<EnemyStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet"))
+        {
+            _enemyStats.enemyHealth -= normalBulletDamage;
+        }
+    }
+}
